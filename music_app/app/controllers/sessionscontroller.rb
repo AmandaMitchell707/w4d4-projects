d43de6ce9f41889fe3fb41e13ab1f870dab09634
Amanda Mitchell <amandamitchell707@gmail.com>
2018-09-13 18:01:12 -0700

    Create views for music_app project

--- music_app/app/controllers/sessionscontroller.rb
+++ music_app/app/controllers/sessionscontroller.rb
@@ -1,24 +1,29 @@
 class SessionsController
+  before_action :require_login, only: [:destroy]
+  before_action :require_logout, only: [:new, :create]
+
   def new
+    @user = User.new
     render :new
   end
 
   def create
-    user = User.find_by_credentials(
+    @user = User.find_by_credentials(
       params[:user][:email]
       params[:user][:password]
     )
 
-    if user
-      login_user!(user)
+    if @user
+      login_user!(@user)
     else
+      @user = User.new
       flash.now[:errors] = ["Invalid user credentials!"]
+      render :new
     end
   end
 
   def destroy
-    current_user.reset_session_token!
-    session[:session_token] = nil
+    logout!
     redirect_to new_session_url
   end
 end
